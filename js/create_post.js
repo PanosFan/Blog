64aa34db19a2596b7c31535bf0fa5ff223a0f5b8
Panagiotis <panfanartzis@gmail.com>
2022-04-16 12:24:54 +0300

    create post

--- js/create_post.js
+++ js/create_post.js
@@ -13,3 +13,26 @@ $("#logoutButton").click(() => {
     },
   });
 });
+
+$("#createPostForm").submit((e) => {
+  e.preventDefault();
+  $.ajax({
+    url: "http://localhost:80/Blog/api/postsApi.php",
+    type: "POST",
+    data: JSON.stringify({
+      title: $("#title").val(),
+      body: $("#floatingTextarea").val(),
+    }),
+    success: () => {
+      $(location).prop("href", "http://localhost:5500/home.html");
+      $("#title").val("");
+      $("#floatingTextarea").val("");
+    },
+    error: (err) => {
+      alert(err.responseJSON.error);
+    },
+    xhrFields: {
+      withCredentials: true,
+    },
+  });
+});
